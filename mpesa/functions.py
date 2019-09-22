0acb38ce2cb6bcc0fabf1dc8fa7f6bae74ade569
= <ekirapapaul@gmail.com>
2019-09-23 00:33:05 +0300

    [ADD] api functionality

---
+++ mpesa/functions.py
@@ -0,0 +1,128 @@
+import os, socket, json, requests, datetime
+
+from django.urls import reverse
+import requests
+from requests.auth import HTTPBasicAuth
+from base64 import b64encode
+from .models import PaymentTransaction
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
+
+# import env settings
+import importlib
+
+try:
+    dotenv = importlib.import_module('dotenv')
+    dotenv.load_dotenv(dotenv.find_dotenv())
+except ModuleNotFoundError:
+    pass
+
+
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+api_URL = os.environ.get('API_URL')
+INITIATOR_PASS = os.environ.get('INITIATOR_PASS')
+CERTIFICATE_FILE = os.environ.get('CERTIFICATE_FILE')
+HOST_NAME = os.environ.get('HOST_NAME')
+
+
+def encryptInitiatorPassword():
+    PASS = "foobar1234"
+    cert_file_path = os.path.join(os.path.dirname(__file__), CERTIFICATE_FILE)
+    cert_file = open(cert_file_path, 'rb')
+    cert_data = cert_file.read()
+    cert_file.close()
+
+    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+    PASS = str.encode(PASS)  # If you are using python 3
+    pub_key = cert.public_key()
+    cipher = pub_key.encrypt(PASS, padding=PKCS1v15())
+    return b64encode(cipher)
+
+
+def generate_pass_key(cipher):
+    time_now = datetime.datetime.now().strftime("%Y%m%d%H%I%S")
+    PASS_KEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+    shortcode = "601754"
+    s = shortcode + PASS_KEY + time_now
+    encoded = b64encode(s.encode('utf-8')).decode('utf-8')
+
+
+def get_token():
+    api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+    consumer_key = "9ntpVdAfDvNqjt6VzFkbNYbOx5wmKonx"
+    consumer_secret = "Bv7Z7IjRNAetpDAr"
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    jonresponse = json.loads(r.content)
+    access_token = jonresponse['access_token']
+    print(access_token)
+    return access_token
+
+
+def register_url(access_token):
+    api_url = "http://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    headers = {
+        "Authorization": "Bearer %s" % access_token,
+        "Content-Type": "application/json",
+    }
+    request = {
+        "ShortCode": "601754",
+        "ResponseType": "Cancelled",
+        "ConfirmationURL": HOST_NAME + reverse('mpesa-c2b-confirm'),
+        "ValidationURL": HOST_NAME + reverse('mpesa-c2b-validate')
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+
+    print (response.text)
+
+
+def sendSTK(phone_number, amount, orderId=0, transaction_id=None):
+    access_token = get_token()
+    time_now = datetime.datetime.now().strftime("%Y%m%d%H%I%S")
+
+    PASS_KEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+    shortcode = "174379"
+    s = shortcode + PASS_KEY + time_now
+    encoded = b64encode(s.encode('utf-8')).decode('utf-8')
+
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {
+        "Authorization": "Bearer %s" % access_token,
+        "Content-Type": "application/json",
+    }
+    print("Phonenumber: {}, Amount: {}".format(phone_number, amount))
+    request = {
+        "BusinessShortCode": shortcode,
+        "Password": encoded,
+        "Timestamp": time_now,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": str(int(amount)),
+        "PartyA": phone_number,
+        "PartyB": shortcode,
+        "PhoneNumber": phone_number,
+        "CallBackURL": "{}/api/confirm/".format(HOST_NAME),
+        "AccountReference": phone_number,
+        "TransactionDesc": "Payment for {}".format(phone_number)
+    }
+
+    print(json.dumps(request))
+    response = requests.post(api_url, json=request, headers=headers)
+    json_response = json.loads(response.text)
+    print(json_response)
+    if json_response["ResponseCode"] == "0":
+        checkoutId = json_response["CheckoutRequestID"]
+        if transaction_id:
+            transaction = PaymentTransaction.objects.filter(id=transaction_id)
+            transaction.checkoutRequestID = checkoutId
+            transaction.save()
+            return transaction.id
+        else:
+            transaction = PaymentTransaction.objects.create(phone_number=phone_number, checkoutRequestID=checkoutId, amount=amount, order_id=orderId)
+            transaction.save()
+            return transaction.id
+    else:
+        raise Exception("Error sending MPesa stk push", json_response)
--- mpesa/views.py
+++ mpesa/views.py
@@ -1,6 +1,164 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from .functions import sendSTK
+from rest_framework.views import APIView, ListCreateAPIView
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.response import Response
+from .models import PaymentTransaction
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+class PaymentTranactionView(ListCreateAPIView):
+    def post(self, request):
+        return HttpResponse("OK", status=200)
+
+
+class SubmitView(APIView):
+    def post(self, request):
+        data = request.data
+        phone_number = data['phone_number']
+        amount = data['amount']
+        orderId = data['order_id']
+
+        print(phone_number)
+        print(amount)
+        sendSTK(phone_number, amount, orderId)
+        # b2c()
+        message = {"status": "ok"}
+        return Response(message, status=HTTP_200_OK)
+
+
+class CheckTransaction(APIView):
+
+    def post(self, request):
+        data = request.data
+        trans_id = data['transaction_id']
+        try:
+            transaction = PaymentTransaction.objects.filter(id=trans_id).get()
+            if transaction:
+                return JsonResponse({
+                    "message": "ok",
+                    "finished": transaction.isFinished,
+                    "successful": transaction.isSuccessFull
+                },
+                                    status=200)
+            else:
+                # TODO : Edit order if no transaction is found
+                return JsonResponse({
+                    "message": "Error. Transaction not found",
+                    "status": False
+                },
+                                    status=400)
+        except PaymentTransaction.DoesNotExist:
+            return JsonResponse({
+                "message": "Server Error. Transaction not found",
+                "status": False
+            },
+                                status=400)
+
+
+class RetryTransaction(APIView):
+    def post(self, request):
+        trans_id = request.data['transaction_id']
+        try:
+            transaction = PaymentTransaction.objects.filter(id=trans_id).get()
+            if transaction and transaction.isSuccessFull:
+                return JsonResponse({
+                    "message": "ok",
+                    "finished": transaction.isFinished,
+                    "successful": transaction.isSuccessFull
+                },
+                                    status=200)
+            else:
+                response = sendSTK(
+                    phone_number=transaction.phone_number,
+                    amount=transaction.amount,
+                    orderId=transaction.order_id,
+                    transaction_id=trans_id)
+                return JsonResponse({
+                    "message": "ok",
+                    "transaction_id": response
+                },
+                                    status=200)
+
+        except PaymentTransaction.DoesNotExist:
+            return JsonResponse({
+                "message": "Error. Transaction not found",
+                "status": False
+            },
+                                status=400)
+
+
+class ConfirmView(APIView):
+
+    def post(self, request):
+        # save the data
+        print("confirm view reached")
+        request_data = json.dumps(request.data)
+        request_data = json.loads(request_data)
+        body = request_data.get('Body')
+        print("Body " + json.dumps(body))
+        resultcode = body.get('stkCallback').get('ResultCode')
+        # Perform your processing here e.g. print it out...
+        if resultcode == 0:
+            print('Payment successful')
+            requestId = body.get('stkCallback').get('CheckoutRequestID')
+            transaction = PaymentTransaction.objects.get(
+                checkoutRequestID=requestId)
+            if transaction:
+                transaction.isFinished = True
+                transaction.isSuccessFull = True
+                transaction.save()
+
+        else:
+            print('unsuccessfull')
+            requestId = body.get('stkCallback').get('CheckoutRequestID')
+            transaction = PaymentTransaction.objects.get(
+                checkoutRequestID=requestId)
+            if transaction:
+                transaction.isFinished = True
+                transaction.isSuccessFull = False
+                transaction.save()
+
+        # Prepare the response, assuming no errors have occurred. Any response
+        # other than a 0 (zero) for the 'ResultCode' during Validation only means
+        # an error occurred and the transaction is cancelled
+        message = {
+            "ResultCode": 0,
+            "ResultDesc": "The service was accepted successfully",
+            "ThirdPartyTransID": "1237867865"
+        }
+
+        # Send the response back to the server
+        return Response(message, status=HTTP_200_OK)
+
+    def get(self, request):
+        return Response("Confirm callback", status=HTTP_200_OK)
+
+class ValidateView(APIView):
+
+    def post(self, request):
+        # save the data
+        request_data = request.data
+
+        # Perform your processing here e.g. print it out...
+        print("validate data" + request_data)
+
+        # Prepare the response, assuming no errors have occurred. Any response
+        # other than a 0 (zero) for the 'ResultCode' during Validation only means
+        # an error occurred and the transaction is cancelled
+        message = {
+            "ResultCode": 0,
+            "ResultDesc": "The service was accepted successfully",
+            "ThirdPartyTransID": "1234567890"
+        }
+
+        # Send the response back to the server
+        return Response(message, status=HTTP_200_OK)
