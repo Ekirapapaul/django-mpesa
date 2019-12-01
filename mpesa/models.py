f19812cbcc9e704110c348aec72292b1634fc33f
= <ekirapapaul@gmail.com>
2019-12-01 23:03:54 +0300

    Add wallet model

--- mpesa/models.py
+++ mpesa/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.conf import settings
+from .commons.models import BaseModel
 
 # Create your models here.
 class PaymentTransaction(models.Model):
@@ -17,3 +18,14 @@ class PaymentTransaction(models.Model):
 
     def __str__(self):
         return "{} {}".format(self.phone_number, self.amount)
+
+
+class Wallet(BaseModel):
+    phone_number = models.CharField(max_length=30)
+    available_balance = models.DecimalField(('available_balance'), max_digits=6, decimal_places=2, default=0)
+    actual_balance = models.DecimalField(('actual_balance'), max_digits=6, decimal_places=2, default=0)
+    date_modified = models.DateTimeField(auto_now=True, null=True)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.phone_number
