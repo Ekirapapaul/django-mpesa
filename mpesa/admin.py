7d0eef813d2a5b34ec7ab048f87b96d64ebdcbcf
Evans <evans@vorane.com>
2020-08-25 11:30:46 +0300

    TCW add extra fields to PaymentTransaction Admin View

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -5,5 +5,10 @@ from django.contrib import admin
 from .models import PaymentTransaction, Wallet
 
 # Register your models here.
-admin.site.register(PaymentTransaction)
+
+class PaymentTransactionAdmin(admin.ModelAdmin):
+    list_display = ("phone_number", "amount", "isFinished",
+                    "isSuccessFull", "trans_id", 'date_created','date_modified')
+
+admin.site.register(PaymentTransaction, PaymentTransactionAdmin)
 admin.site.register(Wallet)
