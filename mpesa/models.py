bd527b5cec4920c2c8ed8ab87c8246884ae9a2a6
Ekirapapaul <ekirapapaul@gmail.com>
2022-01-21 12:20:34 +0300

    Rename model fields and add isSuccessful and finished in check online api

--- mpesa/models.py
+++ mpesa/models.py
@@ -24,17 +24,17 @@ class BaseModel(models.Model):
 class PaymentTransaction(models.Model):
     phone_number = models.CharField(max_length=30)
     amount = models.DecimalField(max_digits=14, decimal_places=2, default=0)
-    isFinished = models.BooleanField(default=False)
-    isSuccessFull = models.BooleanField(default=False)
+    is_finished = models.BooleanField(default=False)
+    is_successful = models.BooleanField(default=False)
     trans_id = models.CharField(max_length=30)
     order_id = models.CharField(max_length=200)
-    checkoutRequestID = models.CharField(max_length=100)
+    checkout_request_id = models.CharField(max_length=100)
     date_modified = models.DateTimeField(auto_now=True)
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     content_type = models.ForeignKey(ContentType, null=True, blank=True, on_delete=models.SET_NULL)
     object_id = models.PositiveIntegerField(default=0)
-    order_object = GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
         return "{} {}".format(self.phone_number, self.amount)
